e9958421e16f2a7afd2cf2fadbbccd15bd03d28f
Antonio Junior <antoniojrfsousa@gmail.com>
2024-08-28 13:30:51 -0300

    first commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,16 @@
+import { Link} from 'react-router-dom';
+import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
+const Home = () => {
+
+    return (<>
+        <Header />
+        <Button title="Teste 1" />
+        <Button variant="secondary" title="Teste 2"/>
+        <Link to="/login">Fazer Login</Link>
+    
+
+    </>)
+}
+
+export { Home }
